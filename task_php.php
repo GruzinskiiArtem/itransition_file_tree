a11e082dd649199ea841e94044dfb8d2279b1d2d
Artem Gruzinskii <m1nck@yandex.ru>
2017-10-13 18:54:35 +0300

    refactoring

--- task_php.php
+++ task_php.php
@@ -1,35 +1,41 @@
 <?php
+
 declare(strict_types=1);
-showTree();
-function showTree()
-{
-	echo "File is located:".'"'.dirname(__FILE__).'"'."<br>";
-	echo "File tree:"."<br>";
-	$AllFoldersNearby=showAllFoldersNearby(dirname(__FILE__));
-	$fileFloor=0;
-	showAllFilesAndFolders($AllFoldersNearby,dirname(__FILE__),$fileFloor);
+$sapi = php_sapi_name();
+if ($sapi=='cli')
+    $parameterRestrictions = "\r\n";
+else
+    $parameterRestrictions = "<br>";
+showTree($parameterRestrictions);
+
+function showTree(string $parameterRestrictions): void {
+    echo "File is located:".'"'.dirname(__FILE__).'"'.$parameterRestrictions;
+    echo "File tree:"."<br>";
+    $allFoldersNearby=showAllFoldersNearby(dirname(__FILE__));
+    $fileFloor = 0;
+    showAllFilesAndFolders($allFoldersNearby, dirname(__FILE__), $fileFloor, $parameterRestrictions);
 }
-function showAllFilesAndFolders($AllFoldersNearby, $dirname, &$fileFloor)
-{
-	for($i=0; $i<count($AllFoldersNearby); $i++){
-		if($AllFoldersNearby[$i]==="."| $AllFoldersNearby[$i]===".."){
-			continue;
-		}
-		$fail=$dirname.'\\'.$AllFoldersNearby[$i];
-		echo (str_repeat(".....", $fileFloor));
-		if(is_dir($fail)){
-			$fileFloor+=1;
-			echo $fail."<br>";
-			$newAllFoldersNearby=showAllFoldersNearby($fail);
-			showAllFilesAndFolders($newAllFoldersNearby,$fail,$fileFloor);
-		}
-		else{
-			echo $fail."<br>";
-		}
-	}
-	$fileFloor-=1;
+
+function showAllFilesAndFolders(array $allFoldersNearby, string $dirname, int &$fileFloor, string $parameterRestrictions) : void {
+	$allFoldersNearby = array_diff($allFoldersNearby,array(".",".."));
+    foreach ($allFoldersNearby as $element) {
+        $fail = $dirname.'\\'.$element;
+        if (is_dir($fail)) {
+			showAddress($fail, $parameterRestrictions, $fileFloor);
+            $fileFloor += 1;
+            $newAllFoldersNearby=showAllFoldersNearby($fail);
+            showAllFilesAndFolders($newAllFoldersNearby, $fail, $fileFloor, $parameterRestrictions);
+        } else {
+			showAddress($fail, $parameterRestrictions, $fileFloor);
+        }
+    }
+    $fileFloor -= 1;
 }
-function showAllFoldersNearby($fileLocation)
-{
-	return scandir($fileLocation);
+
+function showAllFoldersNearby(string $fileLocation): array {
+    return scandir($fileLocation);
+}
+
+function showAddress(string $fail,string $parameterRestrictions, int $fileFloor): void {
+	echo (str_repeat(".....", $fileFloor)).$fail.$parameterRestrictions;
 }
